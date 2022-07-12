bbdf1674e7e88f475b909b669867dcef4f9d9597
Juyeon703 <sh6244@naver.com>
2022-07-12 17:13:45 +0900

    .

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -14,12 +14,18 @@ public class App {
 
     java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
 
+    String title = "";
+    String content = "";
+    String writer = "";
+    String password = "";
+    
     while (true) {
       System.out.println("메뉴:");
       System.out.println("  1: 게시글 목록");
       System.out.println("  2: 게시글 상세보기");
+      System.out.println("  3: 게시글 등록");
       System.out.println();
-      System.out.print("메뉴를 선택하세요[1..2](0: 종료) ");
+      System.out.print("메뉴를 선택하세요[1..3](0: 종료) ");
 
       int menuNo = keyboardInput.nextInt(); // 저장할 메모리 필요! 익스프레이션이란? 결과를 리턴하는 명령문 변수명은 알아보기 쉽게
       keyboardInput.nextLine(); // 입력한 숫자 뒤에 남아 있는 줄바꿈 코드 제거
@@ -36,7 +42,7 @@ public class App {
         System.out.print('\t');
         System.out.print(20 + "\t");
         System.out.print("홍길동\t");
-        System.out.print("2022-07-08\r\n")
+        System.out.print("2022-07-08\r\n");
 
         System.out.print(2 + "\t" + "제목입니다2\t" + // 출력만
             11 + "\t" + "홍길동\t" + "2022-07-08\n");
@@ -49,14 +55,29 @@ public class App {
       } else if (menuNo == 2) { // else { if {} else {} } => else if {} else{} 대괄호 생략으로 else if 된거임
         System.out.println("[게시글 상세보기]");
         System.out.printf("번호 : %d\n", 1);
-        System.out.printf("제목 : %s\n", "제목입니다1.");
-        System.out.printf("내용 : %s\n", "내용입니다.");
+        System.out.printf("제목 : %s\n", title);
+        System.out.printf("내용 : %s\n", content);
         System.out.printf("조회수 : %d\n", 100);
-        System.out.printf("작성자 : %s\n", "홍길동");
+        System.out.printf("작성자 : %s\n", writer);
         System.out.printf("등록일 : %s\n", "2022-07-28");
+      } else if (menuNo == 3) {
+        System.out.println("[게시글 등록]");
+        System.out.print("제목? ");
+        title = keyboardInput.nextLine();
+
+        System.out.print("내용? ");
+        content = keyboardInput.nextLine();
+
+        System.out.print("작성자? ");
+        writer = keyboardInput.nextLine();
+
+        System.out.print("암호? ");
+        password = keyboardInput.nextLine();
+
       } else {
         System.out.println("메뉴 번호가 옳지 않습니다!");
       }
+      System.out.println(); //메뉴를 처리한 후 빈 줄 출력
     } // while
     System.out.println("안녕히 가세요!");
     keyboardInput.close();
